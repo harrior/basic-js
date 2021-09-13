7d03cef4030c6fb6203167a5e3e3946a87dbceb5
Harrior <harrior@users.noreply.github.com>
2021-09-13 20:26:57 +0300

    feat: dateSample complete

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -7,7 +7,7 @@ const HALF_LIFE_PERIOD = 5730;
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
  * 
- * @param {String} sampleActivity string representation of current activity 
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
@@ -17,7 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+    if (!parseFloat(sampleActivity) || !(typeof(sampleActivity) === 'string'))
+        return false
+    let ln = Math.log(MODERN_ACTIVITY / Number(sampleActivity));
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let t = Math.ceil(ln/k)
+    return t > 0 ? t : false
 }
