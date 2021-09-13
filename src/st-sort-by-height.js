87194058904ac70b0cbd9e06ff0654cd2eab6e8c
Harrior <harrior@users.noreply.github.com>
2021-09-13 20:28:11 +0300

    feat: sortByHeight complete

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,7 +11,23 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+    //throw new NotImplementedError('Not implemented');
+    let i = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === -1)
+            continue
+
+        let min = i
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] === -1)
+                continue
+            if (arr[min] > arr[j])
+                min = j
+        }
+        [arr[i], arr[min]] = [arr[min], arr[i]]
+    }
+    return arr
+
 }
+// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]))
