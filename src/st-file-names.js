85562924fefc0b17ebf524209b32bc18b88042c2
Harrior <harrior@users.noreply.github.com>
2021-09-14 09:36:08 +0300

    feat: renameFiles complete

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,7 +15,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+    let renamedFiles = []
+    for (let name of names) {
+        //check name
+        if (renamedFiles.includes(name)) {
+            //check name[i]
+            for (let i = 1; i < 100500; i++) {
+                let newName = `${name}(${i})`
+                if (!renamedFiles.includes(newName)) {
+                    renamedFiles.push(newName)
+                    break
+                }
+            }
+        } else {
+            renamedFiles.push(name)
+        }
+    }
+    return renamedFiles
 }
