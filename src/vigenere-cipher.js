4f32becad77044207e5f7c5fc8aab4f231b11d68
Harrior <harrior@users.noreply.github.com>
2021-09-14 13:55:21 +0300

    feat: VigenereCipheringMachine complete

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,31 +1,79 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(direct = true) {
+        this.direct = direct;
+        this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+    }
+
+    encrypt(message, key) {
+        if (!message || !key)
+            throw new Error("Incorrect arguments!");
+
+        let M = message.toUpperCase();
+        let K = this.generateKey(M, key);
+        let C = [];
+        let char;
+        for (let i = 0; i < M.length; i++) {
+            if (this.alphabet.indexOf(M[i]) >= 0) {
+                char = this.alphabet[(this.alphabet.indexOf(M[i]) + this.alphabet.indexOf(K[i])) % this.alphabet.length];
+            } else {
+                char = M[i];
+            }
+            C.push(char);
+        }
+        return this.direct? C.join('') : C.reverse().join('')
+    }
+
+    decrypt(message, key) {
+        if (!message || !key)
+            throw new Error("Incorrect arguments!")
+
+        let C = message.toUpperCase();
+        let K = this.generateKey(C, key);
+        let M = [];
+        let char;
+        for (let i = 0; i < C.length; i++) {
+            if (this.alphabet.indexOf(C[i]) >= 0) {
+                char = this.alphabet[(this.alphabet.indexOf(C[i]) - this.alphabet.indexOf(K[i]) + this.alphabet.length) % this.alphabet.length];
+            } else {
+                char = C[i];
+            }
+            M.push(char);
+        }
+        return this.direct ? M.join('') : M.reverse().join('')
+    }
+
+    generateKey(message, key) {
+        let resultKey = ''
+        let shift = 0
+        for (let i = 0; i < message.length; i++) {
+            if (this.alphabet.indexOf(message[i]) === -1){
+                resultKey += ' '
+                shift += 1;
+            } else {
+                resultKey += key[Math.floor((i-shift) % key.length)];
+            }
+        }
+        return resultKey.toUpperCase()
+    }
 }
